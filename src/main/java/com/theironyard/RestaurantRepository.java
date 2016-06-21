83a7d53e45c8ee90e4b81e0cd247b018ffd5032d
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-21 11:55:19 -0400

    create delete

---
+++ src/main/java/com/theironyard/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by michaeldelli-gatti on 6/21/16.
+ */
+public interface RestaurantRepository extends CrudRepository<Restaurant, Integer>{
+
+}
